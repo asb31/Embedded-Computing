88e5d5d310bc6c2bf9758515a1dbaf6abf3c5050
Asb <46268327+asb31@users.noreply.github.com>
2019-12-20 23:27:15 -0500

    Update CodecRLE-com.c
    This file has both. File compressor and a decompressor.

--- CodecRLE-com.c
+++ CodecRLE-com.c
@@ -21,6 +21,184 @@ FILE                  *cpt;
 unsigned char	      *image;
 char	 	     header[80];
 int		        ROWS,COLS,BYTES;
+int		         i,j,b,t,rleon=0,IM=0;
+unsigned char    A,B;
+int    size;
+unsigned char runcout=1;
+unsigned char  ab,C=1,D=0;
+unsigned char a=0;
+
+
+
+
+	
+cpt=fopen(argv[2],"w");
+
+
+
+ fpt=fopen(argv[1],"rb");
+ if (fpt == NULL)
+  {
+  printf("Unable to open %s for reading\n",argv[1]);
+  exit(0);
+  }
+//akshay start here today
+  
+  fseek(fpt,0,SEEK_END);
+  size = ftell(fpt);
+  fseek(fpt,0,SEEK_SET); 
+  image=(unsigned char *)calloc(size,sizeof(unsigned char));
+
+ 
+
+
+
+
+
+  
+  if (image == NULL)
+  {
+  printf("Unable to allocate %d x %d memory\n",COLS,ROWS);
+  exit(0);
+  }
+	/* read image data from file */
+  
+   
+     fread(image,1,size,fpt); 
+  
+ 
+   t=0;
+   A=image[0];
+   
+      
+     for (i=0; i<size; i++)
+     {
+          
+      B=image[i+1];    
+    
+      if (image[i]==image[i+1] || runcout >> 1)
+       {
+          t=0; 
+          if (rleon >> 0)
+           {
+            
+             
+              fwrite(&a,1,1,cpt);
+            rleon=0;
+           }     
+          if (A == B)
+           {
+            if (runcout !=255)
+             {
+              runcout++;
+             }
+            else       
+             {
+              
+                fwrite(&runcout,1,1,cpt);
+                fwrite(&A,1,1,cpt);
+                if (A==0)
+                 {
+                  printf("Akshay is testing here ");
+                 
+                   //fwrite(&C,1,1,cpt);
+                   //fwrite(&D,1,1,cpt);
+                  
+                   //fwrite(&D,1,1,cpt);
+                 }
+             
+              A=B;
+              runcout=1;
+             } 
+           }
+          if (A!=B)
+           {
+            
+            
+                fwrite(&runcout,1,1,cpt);
+                fwrite(&A,1,1,cpt);
+                 
+            A=B;
+            runcout=1;
+            } 
+        }    
+          //CODE END FOR RLE ON
+   
+      else   // CODE START RLE OFF
+      
+       {
+        rleon++;
+        if (t==0)
+         {
+          
+                unsigned char pt = 0;
+                fwrite(&pt,1,1,cpt);
+                fwrite(&A,1,1,cpt);
+                if (A==0)
+                 {
+                 
+                   fwrite(&C,1,1,cpt);
+                   fwrite(&D,1,1,cpt);
+                   fwrite(&D,1,1,cpt);
+                 }
+          
+          A=B;
+         }
+        if (t!=0)
+         {
+          
+           
+                 if (image[i]==0)
+                 {
+                   fwrite(&D,1,1,cpt); 
+                 
+                   fwrite(&C,1,1,cpt);
+                   fwrite(&D,1,1,cpt);
+                   fwrite(&D,1,1,cpt);
+                  }
+                  else
+                   {
+                    fwrite(&image[i],1,1,cpt);
+                   }
+                   
+               
+          A=B;
+         }
+         t++; 
+       }                      //CODE END FOR RLE OFF
+           
+      
+  }
+ }
+
+
+
+
+
+// Code writtem below is for decompressor//
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+Display			*Monitor;
+Window			ImageWindow;
+GC				ImageGC;
+XWindowAttributes		Atts;
+XImage			*Picture;
+
+unsigned char		*displaydata;
+FILE		      *fpt;
+FILE                  *cpt;
+unsigned char	      *image;
+char	 	     header[80];
+int		        ROWS,COLS,BYTES;
 int		        i,t,j,rleon=0;
 unsigned char    A,B,k;
 unsigned char    runcout=1;
